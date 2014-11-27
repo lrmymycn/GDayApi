7d0c840769b9d42ee738481856ec328e8320bd2a
Sam <sam.wang@nways.com.au>
2014-11-28 00:33:20 +1100

    add html to train.php

--- public_html/train.php
+++ public_html/train.php
@@ -5,5 +5,18 @@ $trainTime = new \GDay\Library\Train\TimeTable;
 $timeTable = $trainTime->getNextTrain();
 ?>
 
-Rhodes next train (FROM CITY): <?php echo $timeTable['arrive_time'] ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Next Train</title>
+</head>
+<body>
+    <p>Rhodes next train (FROM CITY): <?php echo $timeTable['arrive_time'] ?></p>
+</body>
+</html>
+
+
 
