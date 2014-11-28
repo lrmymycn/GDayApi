919cedf0b60560e262a6f30b08b35edc6fe20511
Sam <sam.wang@nways.com.au>
2014-11-29 00:37:19 +1100

    add wechat share

--- public_html/train.php
+++ public_html/train.php
@@ -101,7 +101,86 @@ $arriveTime = strtotime($timeTable['arrive_time']);
 <!-- scripts -->
 <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="js/supersized.3.2.7.min.js"></script>
-<script type="text/javascript" src="js/hours.js"></script>
+<script type="text/javascript">
+    // supersized
+    jQuery(function($){
+        $.supersized({
+            // Functionality
+            slideshow               :   1,			// Slideshow on/off
+            autoplay				:	1,			// Slideshow starts playing automatically
+            start_slide             :   1,			// Start slide (0 is random)
+            stop_loop				:	0,			// Pauses slideshow on last slide
+            random					: 	0,			// Randomize slide order (Ignores start slide)
+            slide_interval          :   3000,		// Length between transitions
+            transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+            transition_speed		:	3000,		// Speed of transition
+            new_window				:	1,			// Image links open in new window/tab
+            pause_hover             :   0,			// Pause slideshow on hover
+            keyboard_nav            :   1,			// Keyboard navigation on/off
+            performance				:	2,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+            image_protect			:	1,			// Disables image dragging and right click with Javascript
+
+            // Size || Position
+            min_width		        :   0,			// Min width allowed (in pixels)
+            min_height		        :   0,			// Min height allowed (in pixels)
+            vertical_center         :   1,			// Vertically center background
+            horizontal_center       :   1,			// Horizontally center background
+            fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
+            fit_portrait         	:   1,			// Portrait images will not exceed browser height
+            fit_landscape			:   0,			// Landscape images will not exceed browser width
+
+            // Components
+            slide_links				:	'false',	// Individual links for each slide (Options: false, 'number', 'name', 'blank')
+            thumb_links				:	1,			// Individual thumb links for each slide
+            thumbnail_navigation    :   0,			// Thumbnail navigation
+            slides 					:  	[			// Slideshow Images
+                {image : 'images/background/1.jpg', title : '', thumb : '', url : ''},
+                {image : 'images/background/2.jpg', title : '', thumb : '', url : ''},
+                {image : 'images/background/3.jpg', title : '', thumb : '', url : ''}
+            ]
+
+        });
+    });
+
+    // site loader
+    $(window).load(function() {
+        $('.loader').fadeOut('slow');
+    });
+
+    var onBridgeReady = function () {
+        var SHARE_IMG = 'http://gday.stagingserver.com.au/icon-120x120.png';
+
+        WeixinJSBridge.call('hideToolbar');
+        WeixinJSBridge.on('menu:share:appmessage', function (argv) {
+            WeixinJSBridge.invoke('sendAppMessage', {
+                "img_url": SHARE_IMG,
+                "img_width": "120",
+                "img_height": "120",
+                "link": window.location.href,
+                "desc": 'Next train from Rhodes to Epping will depart in 2 minutes',
+                "title": ''
+            }, function (res) {
+                ;
+            });
+        });
+        WeixinJSBridge.on('menu:share:timeline', function (argv) {
+            WeixinJSBridge.invoke('shareTimeline', {
+                "img_url": SHARE_IMG,
+                "link": window.location.href,
+                "desc": '',
+                "title": 'Next train from Rhodes to Epping will depart in 2 minutes'
+            }, function (res) {
+                ;
+            });
+        });
+    };
+    if (document.addEventListener) {
+        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+    } else if (document.attachEvent) {
+        document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+    }
+</script>
 </body>
 </html>
 
